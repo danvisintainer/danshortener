a9d78101f0af11020e2841238c0eeb22874baeb4
danvisintainer <dan@danvisintainer.com>
2015-05-06 22:05:14 -0400

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'urls#index'
+
+  get '/:shorturl' => 'urls#show'
+
   resources :urls
 
   # The priority is based upon order of creation: first created -> highest priority.
