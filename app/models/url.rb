0dc51335309dfdac29faa507f9fd1ca6e3696252
danvisintainer <dan@danvisintainer.com>
2015-05-07 10:16:46 -0400

    add URL methods to shorten / sanitize URLs

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,27 @@
 class Url < ActiveRecord::Base
+  after_create :init
+
+  def init
+    self.hits = 0
+    sanitize
+    generate_shortcode
+
+    self.save
+  end
+
+  def generate_shortcode
+    # using base36 for now until I figure out base62
+    self.shorturl = id.to_s(36)
+  end
+
+  def sanitize
+    if !self.url.start_with?("http://")
+      self.url = 'http://' + self.url
+    end
+  end
+
+  def add_hit
+    self.hits += 1
+    self.save
+  end
 end
