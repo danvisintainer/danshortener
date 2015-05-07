515c0dbef7b39ab7ff397d9f1b56947c11d5b864
danvisintainer <dan@danvisintainer.com>
2015-05-07 13:52:49 -0400

    implement top 100 list capybara check

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,10 +1,12 @@
 require_relative '../spec_helper.rb'
 
-describe 'url shortening', :type => :feature, js: true do
-  it 'can input a URL and receive a shortened one' do
+describe 'url shortening', :type => :feature do
+  it 'will add the URL to the top 100 list when shortening it' do
     visit '/'
     fill_in("url[url]", :with => "www.google.com")
     click_button('shorten URL')
-    expect(page).to have_content('URL shortened!')
+    visit '/'
+    expect(page).to have_content('www.google.com')
   end
+
 end
